c86964528a1d44aedef16d40c04200020ab62209
Kunal Singh <kunalsin9h@gmail.com>
2025-01-05 22:28:06 +0530

    agent with capabilites of ArXiv and Google Search

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Agent using ReAct Reasoning Framework
 from langgraph.prebuilt import create_react_agent
 from langchain_core.tools import tool
-from langchain_community.tools import GooglePlacesTool
 from langchain_community.utilities import SerpAPIWrapper
+from langchain_community.utilities.arxiv import ArxivAPIWrapper
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from dotenv import load_dotenv
+
+# SERPAPI_API_KEY
+load_dotenv()
 
 @tool
 def search(query: str):
@@ -11,8 +17,31 @@ def search(query: str):
     return search.run(query)
 
 @tool
-def places(query: str):
-    """Use the Google Places API to run a Google Places Query"""
-    places = GooglePlacesTool()
-    return places.run(query)
+def research(query: str):
+    """Use arxiv to search for any query in arxiv e-paper archives"""
+    arxiv = ArxivAPIWrapper(
+            arxiv_search=True,
+            arxiv_exceptions=True,
+            load_all_available_meta=True,
+            ARXIV_MAX_QUERY_LENGTH= 300,
+            top_k_results=3,
+            load_max_docs=3,
+            doc_content_chars_max=40000
+    )
+    return arxiv.run(query)
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp")
+tools = [search, research]
+
+query = input("")
+
+agent = create_react_agent(model, tools)
+input = { "messages": [("human", query)]}
+
+for s in agent.stream(input, stream_mode="values"):
+    message = s["messages"][-1]
+    if isinstance(message, tuple):
+        print(message)
+    else:
+        message.pretty_print()
 
